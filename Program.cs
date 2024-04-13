fa7f3ac10fc4e80a1ad7260c53124ffbe004b62f
PuppyLo <g-rasul@mail.ru>
2024-04-13 09:56:25 +0300

    update

--- Program.cs
+++ Program.cs
@@ -48,148 +48,234 @@ async Task Update(ITelegramBotClient botClient, Update update, CancellationToken
         {
             try
             {
-                Console.WriteLine($"{message.Chat.FirstName}  |  {message.Text}");
+                Console.WriteLine($"{message.Chat.Id}  |  {message.Chat.FirstName}  |  {message.Text}");
 
-                if (message.Text.ToLower() == "/start")
+                try
                 {
-                    await botClient.SendTextMessageAsync(message.Chat.Id, "Здравствуйте! Ознакомьтесь с меню", replyMarkup: replyKeyboardMainMenu);
-                    CheckPrice1 = false;
-                    CheckPrice2 = false;
-                    return;
+                    if (message.Text.ToLower() == "/start")
+                    {
+                        await botClient.SendTextMessageAsync(message.Chat.Id, "Здравствуйте! Ознакомьтесь с меню", replyMarkup: replyKeyboardMainMenu);
+                        CheckPrice1 = false;
+                        CheckPrice2 = false;
+                        return;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    await botClient.SendTextMessageAsync(360528047, ex.ToString()); 
+                    Console.WriteLine(ex.ToString());
                 }
 
-                if (message.Text.ToLower() == "в главное меню")
+                try
                 {
-                    await botClient.SendTextMessageAsync(message.Chat.Id, "Выберите:", replyMarkup: replyKeyboardMainMenu);
-                    CheckPrice1 = false;
-                    CheckPrice2 = false;
-                    return;
+                    if (message.Text.ToLower() == "в главное меню")
+                    {
+                        await botClient.SendTextMessageAsync(message.Chat.Id, "Выберите:", replyMarkup: replyKeyboardMainMenu);
+                        CheckPrice1 = false;
+                        CheckPrice2 = false;
+                        return;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    await botClient.SendTextMessageAsync(360528047, ex.ToString()); 
+                    Console.WriteLine(ex.ToString());
                 }
 
                 #region Price
-                if (message.Text.ToLower().Contains("рассчитать стоимость"))
-                {
-                    await botClient.SendTextMessageAsync(message.Chat.Id, $"{message.Chat.FirstName} ,выберите категорию товаров, если нет вашего товара, пишите для расчета админу.", replyMarkup: replyKeyboardPrice);
 
-                    return;
-                }
-
-                if (message.Text.ToLower().Contains("обувь/верхняя одежда") || message.Text.ToLower().Contains("толстовки/штаны") || message.Text.ToLower().Contains("футболка/шорты") || message.Text.ToLower().Contains("носки/нижнее белье"))
+                try
                 {
                     try
                     {
-                        await botClient.SendPhotoAsync(update.Message.Chat.Id, InputFile.FromUri("https://i.postimg.cc/VLhjfK3G/IMG-20240314-112144.jpg"), caption: "Введите стоимость или заказа в юанях из зеленой кнопки.\n Без учета скидки, так как скидки у всех индивидуальны!");
+                        if (message.Text.ToLower().Contains("рассчитать стоимость"))
+                        {
+                            await botClient.SendTextMessageAsync(message.Chat.Id, $"{message.Chat.FirstName} ,выберите категорию товаров, если нет вашего товара, пишите для расчета админу.", replyMarkup: replyKeyboardPrice);
 
-                        CheckPrice1 = true;
-
-                        return;
+                            return;
+                        }
                     }
-                    catch 
+                    catch (Exception ex)
                     {
-                        await botClient.SendTextMessageAsync(update.Message.Chat.Id, "Введите стоимость или заказа в юанях из зеленой кнопки.\n Без учета скидки, так как скидки у всех индивидуальны!");
-
-                        CheckPrice1 = true;
-
-                        return;
+                        await botClient.SendTextMessageAsync(360528047, ex.ToString()); 
+                        Console.WriteLine(ex.ToString());
                     }
-                }
-                if (message.Text.ToLower().Contains("зимняя/тяжелая обувь"))
-                {
 
                     try
                     {
-                        await botClient.SendPhotoAsync(update.Message.Chat.Id, InputFile.FromUri("https://i.postimg.cc/VLhjfK3G/IMG-20240314-112144.jpg"), caption: "Введите стоимость или заказа в юанях из зеленой кнопки.\n Без учета скидки, так как скидки у всех индивидуальны!");
+                        if (message.Text.ToLower().Contains("обувь/верхняя одежда") || message.Text.ToLower().Contains("толстовки/штаны") || message.Text.ToLower().Contains("футболка/шорты") || message.Text.ToLower().Contains("носки/нижнее белье"))
+                        {
+                            try
+                            {
+                                await botClient.SendPhotoAsync(update.Message.Chat.Id, InputFile.FromUri("https://i.postimg.cc/VLhjfK3G/IMG-20240314-112144.jpg"), caption: "Введите стоимость или заказа в юанях из зеленой кнопки.\n Без учета скидки, так как скидки у всех индивидуальны!");
 
-                        CheckPrice2 = true;
+                                CheckPrice1 = true;
 
-                        return;
+                                return;
+                            }
+                            catch
+                            {
+                                await botClient.SendTextMessageAsync(update.Message.Chat.Id, "Введите стоимость или заказа в юанях из зеленой кнопки.\n Без учета скидки, так как скидки у всех индивидуальны!");
+
+                                CheckPrice1 = true;
+
+                                return;
+                            }
+                        }
                     }
-                    catch
+                    catch (Exception ex)
                     {
-                        await botClient.SendTextMessageAsync(update.Message.Chat.Id, "Введите стоимость или заказа в юанях из зеленой кнопки.\n Без учета скидки, так как скидки у всех индивидуальны!");
+                        await botClient.SendTextMessageAsync(360528047, ex.ToString()); 
+                        Console.WriteLine(ex.ToString());
+                    }
+                    try
+                    {
+                        if (message.Text.ToLower().Contains("зимняя/тяжелая обувь"))
+                        {
 
-                        CheckPrice2 = true;
+                            try
+                            {
+                                await botClient.SendPhotoAsync(update.Message.Chat.Id, InputFile.FromUri("https://i.postimg.cc/VLhjfK3G/IMG-20240314-112144.jpg"), caption: "Введите стоимость или заказа в юанях из зеленой кнопки.\n Без учета скидки, так как скидки у всех индивидуальны!");
 
-                        return;
+                                CheckPrice2 = true;
+
+                                return;
+                            }
+                            catch
+                            {
+                                await botClient.SendTextMessageAsync(update.Message.Chat.Id, "Введите стоимость или заказа в юанях из зеленой кнопки.\n Без учета скидки, так как скидки у всех индивидуальны!");
+
+                                CheckPrice2 = true;
+
+                                return;
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        await botClient.SendTextMessageAsync(360528047, ex.ToString()); 
+                        Console.WriteLine(ex.ToString());
+                    }
+
+                    try
+                    {
+                        if (update.Type == UpdateType.Message && update.Message != null && update.Message.Text != null)
+                        {
+
+                            if (CheckPrice1 || CheckPrice2)
+                            {
+                                var message1 = update.Message.Text;
+                                var price = Convert.ToDouble(message1);
+
+                                try
+                                {
+                                    // Определяем коэффициент в зависимости от значения переменной price
+                                    double coefficient;
+                                    if (price < 500)
+                                    {
+                                        coefficient = CheckPrice1 ? 25 : 34;
+                                    }
+                                    else if (price < 1000)
+                                    {
+                                        coefficient = CheckPrice1 ? 16.2 : 18;
+                                    }
+                                    else
+                                    {
+                                        coefficient = CheckPrice1 ? 15.3 : 16.2;
+                                    }
+
+                                    price *= coefficient; // Умножаем стоимость на коэффициент
+
+                                    CheckPrice1 = false;
+                                    CheckPrice2 = false;
+
+                                    await botClient.SendTextMessageAsync(update.Message.Chat.Id, $"Стоимость доставки будет {Math.Round(price, 1)}", replyMarkup: replyKeyboardMainMenu);
+                                }
+                                catch
+                                {
+                                    await botClient.SendTextMessageAsync(update.Message.Chat.Id, "Введите стоимость вашего заказа", replyMarkup: replyKeyboardMainMenu);
+                                }
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        await botClient.SendTextMessageAsync(360528047, ex.ToString()); 
+                        Console.WriteLine(ex.ToString());
                     }
                 }
 
-                if (update.Type == UpdateType.Message && update.Message != null && update.Message.Text != null)
+                catch (Exception ex)
                 {
-
-                    if (CheckPrice1 || CheckPrice2)
-                    {
-                        var message1 = update.Message.Text;
-                        var price = Convert.ToDouble(message1);
-
-                        try
-                        {
-                            // Определяем коэффициент в зависимости от значения переменной price
-                            double coefficient;
-                            if (price < 500)
-                            {
-                                coefficient = CheckPrice1 ? 25 : 34;
-                            }
-                            else if (price < 1000)
-                            {
-                                coefficient = CheckPrice1 ? 16.2 : 18;
-                            }
-                            else
-                            {
-                                coefficient = CheckPrice1 ? 15.3 : 16.2;
-                            }
-
-                            price *= coefficient; // Умножаем стоимость на коэффициент
-
-                            CheckPrice1 = false;
-                            CheckPrice2 = false;
-
-                            await botClient.SendTextMessageAsync(update.Message.Chat.Id, $"Стоимость доставки будет {Math.Round(price, 1)}", replyMarkup: replyKeyboardMainMenu);
-                        }
-                        catch
-                        {
-                            await botClient.SendTextMessageAsync(update.Message.Chat.Id, "Введите стоимость вашего заказа", replyMarkup: replyKeyboardMainMenu);
-                        }
-                    }
+                    await botClient.SendTextMessageAsync(360528047, ex.ToString()); 
+                    Console.WriteLine(ex.ToString());
                 }
 
                 #endregion
 
-                if (message.Text.ToLower().Contains("отзывы"))
+                try
                 {
-                    await botClient.SendTextMessageAsync(message.Chat.Id, "Тут будет ссылка на отзывы ...", replyMarkup: replyKeyboardMainMenu);
-                    return;
+                    if (message.Text.ToLower().Contains("отзывы"))
+                    {
+                        await botClient.SendTextMessageAsync(message.Chat.Id, "Тут будет ссылка на отзывы ...", replyMarkup: replyKeyboardMainMenu);
+                        return;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    await botClient.SendTextMessageAsync(360528047, ex.ToString()); 
+                    Console.WriteLine(ex.ToString());
                 }
 
-                if (message.Text.ToLower().Contains("связаться с админом"))
+                try
                 {
-                    await botClient.SendTextMessageAsync(message.Chat.Id, "[@Heyrano](tg://user?id=713706717) или [@bugzis](tg://user?id=808426527)", replyMarkup: replyKeyboardMainMenu, parseMode: ParseMode.MarkdownV2);
-                    return;
+                    if (message.Text.ToLower().Contains("связаться с админом"))
+                    {
+                        await botClient.SendTextMessageAsync(message.Chat.Id, "[@Heyrano](tg://user?id=713706717) или [@bugzis](tg://user?id=808426527)", replyMarkup: replyKeyboardMainMenu, parseMode: ParseMode.MarkdownV2);
+                        return;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    await botClient.SendTextMessageAsync(360528047, ex.ToString()); 
+                    Console.WriteLine(ex.ToString());
                 }
 
-                if (message.Text.ToLower().Contains("скачать poizon"))
+                try
                 {
-                    await botClient.SendTextMessageAsync(message.Chat.Id, "Выберите ваше платформу и перейдите по ссылке для загрузки \n <a href=\"https://www.anxinapk.com/rj/12201303.html\">Android</a> или <a href=\"https://apps.apple.com/us/app/得物-得到运动x潮流x好物/id1012871328\">iPhone</a>", replyMarkup: replyKeyboardMainMenu, parseMode: ParseMode.Html, disableWebPagePreview: true);
-                    return;
+                    if (message.Text.ToLower().Contains("скачать poizon"))
+                    {
+                        await botClient.SendTextMessageAsync(message.Chat.Id, "Выберите ваше платформу и перейдите по ссылке для загрузки \n <a href=\"https://www.anxinapk.com/rj/12201303.html\">Android</a> или <a href=\"https://apps.apple.com/us/app/得物-得到运动x潮流x好物/id1012871328\">iPhone</a>", replyMarkup: replyKeyboardMainMenu, parseMode: ParseMode.Html, disableWebPagePreview: true);
+                        return;
+                    }
                 }
-
+                catch (Exception ex)
+                {
+                    await botClient.SendTextMessageAsync(360528047, ex.ToString()); 
+                    Console.WriteLine(ex.ToString());
+                }
             }
+
             catch (Exception ex)
             {
+                await botClient.SendTextMessageAsync(360528047, ex.ToString()); 
                 Console.WriteLine(ex.ToString());
             }
         }
 
 
     }
+
     catch (Exception ex)
     {
+        await botClient.SendTextMessageAsync(360528047, ex.ToString()); 
         Console.WriteLine(ex.ToString());
     }
 }
 
-async Task Error(ITelegramBotClient client, Exception exception, CancellationToken token)
+async Task Error(ITelegramBotClient botClient, Exception ex, CancellationToken token)
 {
-    throw new NotImplementedException();
+    await botClient.SendTextMessageAsync(360528047, ex.ToString());
+    Console.WriteLine(ex.ToString());
 }
 
